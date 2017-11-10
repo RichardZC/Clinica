554fdc032fbc152bdfed6e310819f626fcbb8b55
RichardLaptop <r_zapatac@hotmail.com>
2017-11-10 09:22:05 -0500

    base
    base

---
+++ Web/js/app.js
@@ -0,0 +1,11 @@
+﻿var vm = new Vue({
+    el: '#miapp',
+    data: {
+        message: 'Hello'
+    },
+    computed: {
+        reversedMessage: function () {
+            return this.message.split('').reverse().join('')
+        }
+    }
+})
